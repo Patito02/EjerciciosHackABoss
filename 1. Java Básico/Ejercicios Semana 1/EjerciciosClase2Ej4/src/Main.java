eed816d29e4e74720a0fce40f1c2aa222dd81e1f
Patito02 <patriciabaez1987@gmail.com>
2024-02-22 15:16:02 +0100

    Mejora ej clase 2 ej 4

--- 1. Java Básico/Ejercicios Semana 1/EjerciciosClase2Ej4/src/Main.java
+++ 1. Java Básico/Ejercicios Semana 1/EjerciciosClase2Ej4/src/Main.java
@@ -9,30 +9,36 @@ public class Main {
         String mapa[][] = new String[5][5];
         Scanner scanner = new Scanner(System.in);
         Scanner scanner2 = new Scanner(System.in);
-
+        // agrego las variables "filas" y "columnas" para poder trabajar con distintos
+        // tamaños de matriz
+        int filas = mapa.length;
+        int columnas=0;
         for(int i=0; i< mapa.length;i++){
             for(int j=0; j<mapa[i].length;j++){
                 mapa[i][j] = "O";
+                if(i==0){
+                    columnas++;
+                }
             }
         }
         System.out.println("----- Bienvenid@ al sistema de reservas del teatro -----");
         while (!bandera){
-            System.out.println("Por favor elija una fila del 1 al 5:");
+            System.out.println("Por favor elija una fila del 1 al "+filas+":");
             fila = scanner.nextInt();
-            System.out.println("Por favor elija un asiento del 1 al 5:");
+            System.out.println("Por favor elija un asiento del 1 al "+columnas+":");
             asiento = scanner.nextInt();
 
             if(fila <= 0){
-                System.out.println("Error en el número de fila. Elija un fila del 1 al 5.");
+                System.out.println("Error en el número de fila. Elija un fila del 1 al "+filas);
             } else if(asiento <=0){
-                System.out.println("Error en el número de asiento. Elija un asiento del 1 al 5");
-            } else if(fila <= mapa.length && asiento <= mapa.length){
+                System.out.println("Error en el número de asiento. Elija un asiento del 1 al "+columnas);
+            } else if(fila <= filas && asiento <= columnas){
                 if(mapa[fila-1][asiento-1].equals("O")){
                     System.out.println("Asiento reservado correctamente.");
                     mapa[fila-1][asiento-1] = "X";
 
-                    for(int i=0; i< mapa.length;i++){
-                        for(int j=0; j<mapa[i].length;j++){
+                    for(int i=0; i<filas;i++){
+                        for(int j=0; j<columnas;j++){
                             System.out.print(" " +mapa[i][j] + " ");
                         }
                         System.out.println("");
@@ -40,10 +46,10 @@ public class Main {
                 } else {
                     System.out.println("Lo lamento, el asiento ya está ocupado.");
                 }
-            } else if (fila > mapa.length){
-                System.out.println("Error en el número de fila. Elija un fila del 1 al 5.");
+            } else if (fila > filas){
+                System.out.println("Error en el número de fila. Elija un fila del 1 al "+filas);
             } else {
-                System.out.println("Error en el número de asiento. Elija un asiento del 1 al 5");
+                System.out.println("Error en el número de asiento. Elija un asiento del 1 al "+columnas);
             }
 
             System.out.println("Desea reservar otro asiento? Elija SI/NO");
