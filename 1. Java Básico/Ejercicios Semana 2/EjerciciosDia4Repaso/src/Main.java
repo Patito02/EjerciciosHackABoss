586dabd80f9c7b886c24f53c22fc01e87b4c5be1
Patito02 <patriciabaez1987@gmail.com>
2024-02-26 13:07:19 +0100

    Semana 2 Ej Dia 4 Repaso condigna C, D e informe.

--- 1. Java Básico/Ejercicios Semana 2/EjerciciosDia4Repaso/src/Main.java
+++ 1. Java Básico/Ejercicios Semana 2/EjerciciosDia4Repaso/src/Main.java
@@ -12,8 +12,55 @@ public class Main {
 
         //consigna B
         for(int i=0; i< vectorVerduras.length; i++){
-            System.out.println(vectorVerduras[i].getNombre() + " posee " + vectorVerduras[i].getCalorias()+" calorias");
+            System.out.println(vectorVerduras[i].getNombre() + " posee " + vectorVerduras[i].getCalorias()+" calorías");
         }
 
+        //consigna C
+        Verdura verdu1 = vectorVerduras[1];
+        Verdura verdu2 = vectorVerduras[3];
+        System.out.println("\n--------Antes----------");
+        System.out.println("id: " +verdu1.getId()+", nombre: "+ verdu1.getNombre() +
+                ", color: "+ verdu1.getColor() + ", calorías: " + verdu1.getCalorias()+
+                ", debe cocinarse?: "+verdu1.isDebeCocinarse());
+        System.out.println("id: " +verdu2.getId()+", nombre: "+ verdu2.getNombre() +
+                ", color: "+ verdu2.getColor() + ", calorías: " + verdu2.getCalorias()+
+                ", debe cocinarse?: "+verdu2.isDebeCocinarse());
+
+        verdu1.setId(6);
+        verdu1.setNombre("puerro");
+        verdu1.setColor("verde");
+        verdu1.setCalorias(40);
+        verdu1.setDebeCocinarse(true);
+        verdu2.setId(7);
+        verdu2.setNombre("calabaza");
+        verdu2.setColor("naranja");
+        verdu2.setCalorias(45);
+        verdu2.setDebeCocinarse(true);
+
+        System.out.println("--------Después----------");
+        System.out.println("id: " +verdu1.getId()+", nombre: "+ verdu1.getNombre() +
+                ", color: "+ verdu1.getColor() + ", calorías: " + verdu1.getCalorias()+
+                ", debe cocinarse?: "+verdu1.isDebeCocinarse());
+        System.out.println("id: " +verdu2.getId()+", nombre: "+ verdu2.getNombre() +
+                ", color: "+ verdu2.getColor() + ", calorías: " + verdu2.getCalorias()+
+                ", debe cocinarse?: "+verdu2.isDebeCocinarse());
+
+        //consigna D
+        int cantidad = 0;
+        String color = "verde";
+        System.out.println("\nLas verduras de color "+color+" son: ");
+        for(int i=0; i< vectorVerduras.length; i++){
+            Verdura verdu = vectorVerduras[i];
+            if(verdu.getColor().equalsIgnoreCase(color)){
+                cantidad++;
+
+                System.out.println("id: " +verdu.getId()+", nombre: "+ verdu.getNombre() +
+                        ", color: "+ verdu.getColor() + ", calorías: " + verdu.getCalorias()+
+                        ", debe cocinarse?: "+verdu.isDebeCocinarse());
+            }
+        }
+        if(cantidad == 0){
+            System.out.println("\nNo hay verduras de color "+ color +" en este listado.");
+        }
     }
 }
